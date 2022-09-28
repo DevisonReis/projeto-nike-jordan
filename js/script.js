0a3c15e3b4ecd25ef0f155da3c430975e3f6a33d
Devison Reis <devisonreis@hotmail.com>
2022-09-28 15:13:59 -0300

    projeto Nike Jordan - página inicial + página de detalhes do produto v1.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
-function alteraImagem() {
+function primeiraImagem() {
+    document.getElementById("foto").src = "../imagens/1.png";
+}
+
+function segundaImagem() {
     document.getElementById("foto").src = "../imagens/2.png";
+}
+
+function terceiraImagem() {
+    document.getElementById("foto").src = "../imagens/3.png";
+}
+
+function quartaImagem() {
+    document.getElementById("foto").src = "../imagens/4.png";
+}
+
+function quintaImagem() {
+    document.getElementById("foto").src = "../imagens/5.png";
+}
+
+function sextaImagem() {
+    document.getElementById("foto").src = "../imagens/6.png";
+}
+
+function setimaImagem() {
+    document.getElementById("foto").src = "../imagens/7.png";
+}
+
+function oitavaImagem() {
+    document.getElementById("foto").src = "../imagens/8.png";
 }
